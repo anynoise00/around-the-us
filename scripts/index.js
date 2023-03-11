fdf67fde568c169a3bc1e5e06ef42c116db28fbd
anynoise00 <anynoise00@gmail.com>
2023-03-11 18:34:39 -0300

    fix: Handle form submit the right way

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,6 @@
 const editForm = document.querySelector(".edit-form");
 const editFormInputName = editForm.querySelector(".edit-form__field[name='name']");
 const editFormInputDesc = editForm.querySelector(".edit-form__field[name='description']");
-const submitFormBtn = editForm.querySelector(".edit-form__button-submit");
 const closeFormBtn = editForm.querySelector(".edit-form__button-close");
 
 const profile = document.querySelector(".profile");
@@ -11,7 +10,7 @@ const editProfileBtn = profile.querySelector(".profile__button-edit");
 
 editProfileBtn.addEventListener("click", openEditForm);
 closeFormBtn.addEventListener("click", closeEditForm);
-submitFormBtn.addEventListener("click", submitForm);
+editForm.addEventListener("submit", handleEditFormSubmit);
 
 function openEditForm(ev) {
   ev.preventDefault();
@@ -27,7 +26,7 @@ function closeEditForm(ev) {
   editForm.classList.remove("edit-form_visible");
 }
 
-function submitForm(ev) {
+function handleEditFormSubmit(ev) {
   ev.preventDefault();
 
   profileName.textContent = editFormInputName.value;
