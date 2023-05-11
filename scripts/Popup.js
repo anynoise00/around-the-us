e437aed2ee607f6c919d618c37a3697a8431f1e0
anynoise00 <anynoise00@gmail.com>
2023-05-11 15:40:50 -0300

    refactor: Create Popup class

---
+++ scripts/Popup.js
@@ -0,0 +1,31 @@
+export default class Popup {
+  constructor(popupSelector) {
+    this._element = document.querySelector(popupSelector);
+  }
+
+  open() {
+    this._element.classList.add("popup_visible");
+  }
+
+  close() {
+    this._element.classList.remove("popup_visible");
+  }
+
+  setEventListeners() {
+    const closeBtn = this._element.querySelector("popup__button-close");
+    closeBtn.addEventListener("click", () => {
+      this.close();
+    });
+
+    const popupOverlay = this._element.querySelector(".popup__overlay");
+    popupOverlay.addEventListener("click", () => {
+      this.close();
+    });
+  }
+
+  _handleEscClose(key) {
+    if (key === "Escape") {
+      this.close();
+    }
+  }
+}
